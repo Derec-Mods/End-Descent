ea9eda5e09c94dc91eb85f98f8b00654b7b855b0
Derek <derex1987@gmail.com>
2025-11-10 23:03:48 -0600

    feat: startup logs

--- src/main/java/io/github/derec4/endToOverworld/EndToOverworld.java
+++ src/main/java/io/github/derec4/endToOverworld/EndToOverworld.java
@@ -2,6 +2,7 @@ package io.github.derec4.endToOverworld;
 
 import io.github.derec4.endToOverworld.listeners.EndVoidListener;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EndToOverworld extends JavaPlugin {
@@ -10,6 +11,14 @@ public final class EndToOverworld extends JavaPlugin {
     public void onEnable() {
         // Inspiration from FunkyToc
         Bukkit.getPluginManager().registerEvents(new EndVoidListener(this), this);
+
+        Bukkit.getLogger().info("");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|                             " +
+                "  ");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  | Derex |     End to Overworld v" + getDescription().getVersion());
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|     Inspiration from FunkyToc");
+        Bukkit.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  |_______|     Running on " + Bukkit.getName() + " - " + Bukkit.getVersion());
+        Bukkit.getLogger().info("");
     }
 
     @Override
